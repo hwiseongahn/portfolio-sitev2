df3794c1cddc4cfa0d5598e4fbb5a947073c9a12
Hwiseong <hw969655@dal.ca>
2025-05-06 02:08:56 -0300

    added page title to gallery

--- src/app/gallery/page.js
+++ src/app/gallery/page.js
@@ -1,6 +1,7 @@
 "use client"
 import Image from "next/image";
 import { ImageGallery } from "react-image-grid-gallery";
+import PageTitle from '@/components/page-title'
 
 const imagesArray = [
   {
@@ -12,23 +13,28 @@ const imagesArray = [
 
 export default function Gallery() {
   return (
-    <main>
-        <ImageGallery
-            imagesInfoArray={imagesArray}
-            columnCount={"auto"}
-            columnWidth={280}
-            gapSize={22}
-        />
-        <p>Will be regularly updated (2024-08-13)</p>
-        <Image
-            width={1512}
-            height={550}
-            className='absolute left-1/2 top-0 -z-10 -translate-x-1/2'
-            src='/gradient-background-top.png'
-            alt=''
-            role='presentation'
-            priority
-          />
-    </main>
+    <>
+      <PageTitle
+        title = 'Gallery'
+        description = 'A collection of my photos.'
+        animate = {true}
+      />
+      <ImageGallery
+          imagesInfoArray={imagesArray}
+          columnCount={"auto"}
+          columnWidth={280}
+          gapSize={22}
+      />
+      <p>A Gallery</p>
+      {/* <Image
+          width={1512}
+          height={550}
+          className='absolute left-1/2 top-0 -z-10 -translate-x-1/2'
+          src='/gradient-background-top.png'
+          alt=''
+          role='presentation'
+          priority
+      /> */}
+    </>
   )
 }
--- src/app/projects/page.js
+++ src/app/projects/page.js
@@ -2,38 +2,38 @@
 
 import ProjectCards from '@/components/project-cards'
 import { motion } from 'framer-motion'
-import PageTitle, {pageTitle} from '@/components/page-title'
+import PageTitle from '@/components/page-title'
 
 export default function page() {
-    const projects = [
-        {
-            slug: 'SummBot',
-            name: 'SummBot',
-            description: 'Discord bot for summarizing conversations',
-            src: 'summbotss.jpg',
-            techstack: ['React', 'Next.js', 'Tailwind CSS'],
-            selected: true,
-            repoUrl: 'https://github.com/hwiseongahn/summdiscordbot'
-        },
-        {
-          slug: 'Algae and CO2',
-          name: 'Algae and CO₂ Heat Map',
-          description: 'NASA Space Apps Challenge 2024',
-          src: 'co2ss.png',
-          techstack: ['React', 'Next.js', 'Tailwind CSS'],
-          selected: true,
-          repoUrl: 'https://github.com/hwiseongahn/NASAcommunitymapping'
-        },
-        {
-          slug: 'ClearCal',
-          name: 'ClearCal',
-          description: 'Mental Health and Generative AI Hackathon',
-          src: 'ClearCal.png',
-          techstack: ['React', 'Next.js', 'Tailwind CSS'],
-          selected: true,
-          repoUrl: 'https://github.com/hwiseongahn/genaihackathon'
-        },
-      ];
+  const projects = [
+    {
+    slug: 'SummBot',
+    name: 'SummBot',
+    description: 'Discord bot for summarizing conversations',
+    src: 'summbotss.jpg',
+    techstack: ['Python', 'discord.py', 'Google Gemini', 'AWS'],
+    selected: true,
+    repoUrl: 'https://github.com/hwiseongahn/summdiscordbot'
+    },
+    {
+        slug: 'Algae and CO2',
+        name: 'Algae and CO₂ Heat Map',
+        description: 'NASA Space Apps Challenge 2024',
+        src: 'co2ss.png',
+        techstack: ['Python', 'Flask', 'Folium', 'JavaScript'],
+        selected: true,
+        repoUrl: 'https://github.com/hwiseongahn/NASAcommunitymapping'
+    },
+    {
+      slug: 'ClearCal',
+      name: 'ClearCal',
+      description: 'Mental Health and Generative AI Hackathon',
+      src: 'ClearCal.png',
+      techstack: ['React', 'Node.js', 'Bootstrap', 'Google Gemini'],
+      selected: true,
+      repoUrl: 'https://github.com/hwiseongahn/genaihackathon'
+    },
+  ];
     return (
       <>
         {/* <script
@@ -42,7 +42,7 @@ export default function page() {
         /> */}
         <PageTitle
             title='Projects'
-            description='A collection of my projects and experiments.'
+            description='A collection of my projects.'
             animate={true}
         />
         <motion.div
