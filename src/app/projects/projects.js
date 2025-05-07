f374fc3a9b6c3b9bf10ff9b342012b645150b98a
Hwiseong <hw969655@dal.ca>
2025-05-07 19:59:44 -0300

    added paragraph for Algae Co2Paragraph

--- src/app/projects/projects.js
+++ src/app/projects/projects.js
@@ -1,5 +1,7 @@
 const summbotParagraph = `SummBot is a Discord bot that utilizes Google Gemini to summarize conversations in real-time. It is designed to help users keep track of important discussions and highlights within their Discord servers. The bot can be easily added to any server and provides a user-friendly interface for accessing summaries.`;
 const ClearCalParagraph = `ClearCal is a mental health application that leverages generative AI to provide personalized support and resources. It offers users a platform to track their mental well-being, access helpful content, and engage with AI-driven tools for self-improvement. The app is designed to be user-friendly and accessible, making mental health support available to everyone.`;
+const AlgaeandCO2Paragraph = `This project is a web application that visualizes the relationship between algae blooms and CO₂ levels in the ocean. It uses data from NASA's Earth Observing System Data and Information System (EOSDIS) to create an interactive heat map. Users can explore the data by selecting different time periods and locations, allowing them to see how algae blooms and CO₂ levels change over time.`;
+
 export const projects = [
 {
 slug: 'SummBot',
@@ -20,7 +22,8 @@ src: 'co2ss.png',
 techstack: ['Python', 'Flask', 'Folium', 'JavaScript'],
 selected: true,
 repoUrl: 'https://github.com/hwiseongahn/NASAcommunitymapping',
-url: 'https://algaeandco2.vercel.app/'
+url: 'https://algaeandco2.vercel.app/',
+paragraph: AlgaeandCO2Paragraph,
 },
 {
 slug: 'ClearCal',
