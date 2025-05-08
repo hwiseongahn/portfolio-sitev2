78722236ca390b9207fdac992c61e93f04b84b30
Hwiseong <hw969655@dal.ca>
2025-05-08 14:04:02 -0300

    added about

--- src/components/aboutMe.js
+++ src/components/aboutMe.js
@@ -30,9 +30,9 @@ const AboutMe = () => {
 
   const router = useRouter();
 
-  // const handleClick = () => {
-  //   router.push('/about');
-  // };
+  const handleClick = () => {
+    router.push('/about');
+  };
 
   return (
     <motion.div
@@ -86,11 +86,11 @@ const AboutMe = () => {
         </div>
       </motion.div>
       <div className="my-8 flex items-center justify-center">
-        <Link href="https://www.linkedin.com/in/hwiseongahn/" target="_blank" rel="noopener noreferrer">
-          <Button variant="outline">
+
+          <Button variant="outline" onClick={handleClick}>
             Know more about me
           </Button>
-        </Link>
+
       </div>
     </motion.div>
   );
