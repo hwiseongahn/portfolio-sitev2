7d831cf546e91e97faab4baca4fdf282db265cec
Hwiseong <hw969655@dal.ca>
2025-05-10 23:40:31 -0300

    changing generatemetadata cuz it didn't load in discord

--- src/app/[locale]/page.js
+++ src/app/[locale]/page.js
@@ -4,6 +4,39 @@ import Hero from "@/components/hero";
 import Contact from "@/components/contact";
 import { Timeline } from "@/components/timeline";
 
+export const dynamic = "force-static";
+
+export async function generateMetadata() {
+  return {
+    title: "Hwiseong Ahn",
+    description: "Hwiseong's Portfolio",
+    openGraph: {
+      title: "Hwiseong Ahn",
+      description: "Portfolio of Hwiseong Ahn",
+      url: "https://hwiportfolio.vercel.app/en",
+      siteName: "Hwiseong Ahn",
+      images: [
+        {
+          url: "https://hwiportfolio.vercel.app/preview.jpg",
+          width: 1200,
+          height: 630,
+        },
+      ],
+      locale: "en_US",
+      type: "website",
+    },
+    twitter: {
+      card: "summary_large_image",
+      title: "Hwiseong Ahn",
+      description: "Portfolio of Hwiseong Ahn",
+      images: ["https://hwiportfolio.vercel.app/preview.jpg"],
+    },
+    icons: {
+      icon: "/favicon.ico",
+    },
+  };
+}
+
 export default function Home() {
   return (
     <>
